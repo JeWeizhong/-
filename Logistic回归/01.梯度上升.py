cf5090bf48cd0852df7db1aa42d135cec4439981
JeWeizhong <zhmqsnq90@foxmail.com>
2018-10-08 17:20:31 +0800

    梯度上升

--- Logistic回归/01.梯度上升.py
+++ Logistic回归/01.梯度上升.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 
 def plotBestFit(weights):
     '''
-    画出决策边界
+    画出决策边界,matplotlib暂时看不懂
     '''
+    #  读取数据
     dataMat,labelMat = loadDataSet()
+    # 转成矩阵
     dataArr = np.array(dataMat)
+    # 获取形状
     n = np.shape(dataMat)[0]
     xcord1 = []; ycord1 = []
     xcord2 = []; ycord2 = []
+    # 根据label画出不同的颜色
     for i in range(n):
         if int(labelMat[i])== 1:
             xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
@@ -22,6 +26,7 @@ def plotBestFit(weights):
     ax.scatter(xcord2, ycord2, s=30, c='green')
     x = np.arange(-3.0, 3.0, 0.1)
     y = (-weights[0]-weights[1]*x)/weights[2]
+    # 画分类线
     ax.plot(x, y)
     plt.xlabel('X1'); plt.ylabel('X2');
     plt.show()
@@ -32,10 +37,11 @@ def gradAscent(dataIn,classLables):
     '''
     # np.mat : 转换为矩阵类型
     dataMaxtrix = np.mat(dataIn)
-    # 转置，用于多维数组，一二维的使用 np.T
+    # 转置成为列向量
     labelMat = np.mat(classLables).transpose()
     # 返回矩阵的形状
     m,n = np.shape(dataMaxtrix)
+    # 固定阿尔法值
     alpha = 0.001
     maxCycles = 500 
     # 全1 矩阵
@@ -55,9 +61,14 @@ def sigmoid(inX):
     return 1.0/(1+np.exp(-inX))
 
 def loadDataSet():
+    '''
+    导入数据
+    '''
     dataMat = []; labelMat = []
+    # 文件路径，不知道是什么文件，先用着就是
     fr = open('F:\\zhongjianwei\\pyscript\\机器学习实战\\Machine-learning-in-action-notes-and-code\\Logistic回归\\Ch05\\testSet.txt')
     for line in fr.readlines():
+        # 默认分隔符： 换行，空格，制表符
         lineArr = line.strip().split()
         dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
         labelMat.append(int(lineArr[2]))
@@ -66,7 +77,9 @@ def loadDataSet():
 def main():
     dataArr,labelMat = loadDataSet()
     weights = gradAscent(dataArr,labelMat)
-    plotBestFit(weights.getA()) #　降维
+    print(weights)
+    # 矩阵转换成数组
+    #plotBestFit(weights.getA())
 
 if __name__ == '__main__':
     main()
